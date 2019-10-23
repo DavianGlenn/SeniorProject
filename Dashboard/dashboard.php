9d9d8b2709cf5ef290e2789e421510c659375172
Bria <bpmassey@aggies.ncat.edu>
2019-10-23 13:14:58 -0400

    UI Changes/Data Visualization Updates
    Changed the Desgin of website
    Added the key functions of website

--- Dashboard/dashboard.php
+++ Dashboard/dashboard.php
@@ -162,7 +162,7 @@ html,body,h1,h2,h3,h4,h5 {font-family: "Raleway", sans-serif}
 
   <div class="w3-panel">
     <div class="w3-row-padding" style="margin:0 -16px">
-      
+
       <div class="w3-twothird">
         <h5>Feeds</h5>
         <table class="w3-table w3-striped w3-white">
